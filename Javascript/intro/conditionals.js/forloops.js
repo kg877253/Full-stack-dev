6332412836dd2702b8b554e1754e056086522b2c
Kartik Gupta <kg877253@gmail.com>
2026-07-02 17:23:40 +0530

    for loop ,for in ,for of three types of for loop.

---
+++ Javascript/intro/conditionals.js/forloops.js
@@ -0,0 +1,19 @@
+let obj={
+    name:"Kartik",
+    role:"Full stack developer",
+    age:25
+}
+//for in loop is used to iterate over the properties of an object
+for (const key in obj) {
+    
+    const element = obj[key];
+    console.log(key,element);
+    
+    
+}
+//for of loop is used to iterate over the values of an iterable object like array or string
+
+for (const c of "KartiK") {
+    // console.log(c.toLowerCase());
+    console.log(c)
+}
