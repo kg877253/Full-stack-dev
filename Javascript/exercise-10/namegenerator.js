34999e956a234d1ee7d2e9a7f3729f7253a356c9
Kartik Gupta <kg877253@gmail.com>
2026-07-04 14:57:45 +0530

    2-methods for solving random name generator problem

--- Javascript/exercise-10/namegenerator.js
+++ Javascript/exercise-10/namegenerator.js
@@ -1,3 +1,27 @@
+let obj1={
+    1:"Crazy",
+    2:"Amazing",
+    3:"Fire"
+}
+let obj2={
+    1:"Engine",
+    2:"Foods",
+    3:"Garments"
+}
+let obj3={
+    1:"Bros",
+    2:"Limited",
+    3:"Hub"
+}
+
+let a=Math.floor(Math.random() * 3) + 1;
+let b=Math.floor(Math.random() * 3) + 1;
+let c=Math.floor(Math.random() * 3) + 1;
+
+console.log(obj1[a] , obj2[b] , obj3[c] );
+
+//WE CAN ALSO DO IT LIKE THIS FOR BETTER UNDERSTANDING OF OUR'S AND EASY
+/*
 let a,b,c;
 let adrandom = Math.floor(Math.random() * 3) + 1;
 if (adrandom === 1) {
@@ -32,4 +56,8 @@ else {
     c="Hub";
 }
 
-console.log(`Your new shop name is: ${a} ${b} ${c}`);
+console.log(`Your new shop name is: ${a} ${b} ${c}`);*/
+
+
+
+
