cad4fb11076f9d8bf501f320e142b8562c5c6d7e
Kartik Gupta <kg877253@gmail.com>
2026-07-04 14:45:59 +0530

    correction we can also use this Math.ceil(Math.random() * 3); instead of Math.floor(Math.random() * 3) + 1; but it can give 0 also thats why we dont use it

--- Javascript/exercise-10/namegenerator.js
+++ Javascript/exercise-10/namegenerator.js
@@ -2,10 +2,10 @@ let a;
 let b;
 let c;
 let adrandom = Math.floor(Math.random() * 3) + 1;
-if (adrandom == 1) {
+if (adrandom === 1) {
     a="Crazy";
 }
-else if (adrandom == 2) {
+else if (adrandom === 2) {
     a="Amazing";
 }
 else {
@@ -13,10 +13,10 @@ else {
 }
 
 let shoprandom = Math.floor(Math.random() * 3) + 1;
-if (shoprandom == 1) {
+if (shoprandom === 1) {
     b="Engine";
 }
-else if (shoprandom == 2) {
+else if (shoprandom === 2) {
     b="Foods";
 }
 else {
@@ -24,10 +24,10 @@ else {
 }
 
 let anotherrandom = Math.floor(Math.random() * 3) + 1;
-if (anotherrandom == 1) {
+if (anotherrandom === 1) {
     c="Bros";
 }
-else if (anotherrandom == 2) {
+else if (anotherrandom === 2) {
     c="Limited";
 }
 else {
