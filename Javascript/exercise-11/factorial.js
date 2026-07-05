726b850bf2f41870e9ed2f80c873f98e753dba24
Kartik Gupta <kg877253@gmail.com>
2026-07-05 14:04:29 +0530

    factorial

--- Javascript/exercise-11/factorial.js
+++ Javascript/exercise-11/factorial.js
@@ -1,29 +1,17 @@
-// const factorial = (n) => {
-//     if (n === 0 || n === 1) {
-//         return 1;
-//     }
+let a = 6;
 
-//     return n * factorial(n - 1);
-// };
-
-// // console.log(factorial(6));
-// let a=6;
-// console.log(factorial(a))
-
-// const factorial = (n) => {
-//     if (n === 0 || n === 1) {
-//         return 1;
-//     }
-
-//     return Array.from({ length: n }, (_, i) => i + 1).reduce((acc, curr) => acc * curr, 1);
-// };
-
-// let a = 6;
-// console.log(factorial(a));
-let a=0
 function factorial(number) {
-    let arr= Array.from(Array(number+1).keys())
-    let c=arr.slice(1,).reduce((a,b)=>a*b)
-    return c
+    if (number < 0) {
+        return "Factorial is not defined for negative numbers";
+    }
+
+    let result = 1;
+
+    for (let i = 2; i <= number; i++) {
+        result *= i;
+    }
+
+    return result;
 }
-console.log(factorial(a))
+
+console.log(factorial(a));
--- Javascript/intro/conditionals.js/forloops.js
+++ Javascript/intro/conditionals.js/forloops.js
@@ -3,13 +3,12 @@ let obj={
     role:"Full stack developer",
     age:25
 }
+
 //for in loop is used to iterate over the properties of an object
 for (const key in obj) {
     
     const element = obj[key];
-    console.log(key,":",element);
-    
-    
+    console.log(key,":",element);   
 }
 //for of loop is used to iterate over the values of an iterable object like array or string
 
