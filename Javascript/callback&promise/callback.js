544deb8f164b84d8a68bb5d03a22e394d58fa1e1
Kartik Gupta <kg877253@gmail.com>
2026-07-09 18:36:55 +0530

    callback and promises

---
+++ Javascript/callback&promise/callback.js
@@ -0,0 +1,24 @@
+console.log("helloooo")
+console.log("yo")
+
+// setTimeout(() => {
+//     console.log("i am inside ")
+// },2000)
+
+console.log("i am outside")
+const funcs = ()=>{
+    console.log("i am nothing")
+}
+const callbacks =(arg,funcs)=>{
+    console.log(arg)
+    funcs()
+}
+const loadscript=(src,callback) => {
+    let sr = document.createElement('script')
+    sr.src = src
+    sr.onload = ()=>callbacks("harry", funcs);
+    document.head.append(sr)
+  
+}
+
+loadscript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js",callbacks)
---
+++ Javascript/callback&promise/promise.js
@@ -0,0 +1,25 @@
+console.log("im")
+
+const promis=new Promise((resolve,reject)=>{
+
+    let a= Math.random();
+    //agar a ki value 0.5 se choti h to reject ho jao otherwise resolve ho jao
+    if(a<0.5){
+        reject("not done")
+    }
+    
+    else{
+
+        setTimeout(() => {
+            console.log("i am inside promise")
+            resolve("done")
+            console.log("i am inde promise")
+        },2000)
+    }
+
+
+})
+
+promis.then((value)=>{//then  resolve k andr ki value ko le aata h
+    console.log(value);
+})
