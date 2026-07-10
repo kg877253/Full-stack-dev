0583bde54928ed93f8d129c8620ea412c15558ee
Kartik Gupta <kg877253@gmail.com>
2026-07-10 20:47:21 +0530

    a

--- Javascript/asyncawait/script.js
+++ Javascript/asyncawait/script.js
@@ -1,19 +1,34 @@
-function getdata() {
+async function getdata() {
     return new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("data loaded");
-    }, 3000);   
-})
+        setTimeout(() => {
+            resolve("data loaded");
+        }, 3000);
+    })
 }
 
 
-console.log("starting script...");
+//data load hone tk wait krne k liye async await ka use krte h 
+async function main() {
+    console.log("starting script...");
 
-console.log("reading script...");
+    console.log("reading script...");
 
-console.log("loading data...");
+    console.log("loading data...");
+    
+//DATA ka wait hoga jab tk data load na ho jaye tab tk ye line execute nahi hogi uske bad execute hojayegi age ki lines bhi 
+    let data = await getdata();
+    console.log(data);
 
-let data = getdata();
-console.log(data);
+    console.log("data loaded successfully");
+    console.log("stage 2");
 
-console.log("stage 2")
+}
+
+main();
+
+//this is callback approach to handle the promise returned by getdata function
+// data.then((data) => {
+// console.log(data);
+
+// console.log("stage 2")
+// })
