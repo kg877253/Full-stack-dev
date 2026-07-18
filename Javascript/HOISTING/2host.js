d3d9b72c6b1756cc7ceaaa8a5ce7f103b70d929a
Kartik Gupta <kg877253@gmail.com>
2026-07-18 15:55:37 +0530

    HOISTING

--- Javascript/HOISTING/2host.js
+++ Javascript/HOISTING/2host.js
@@ -1,16 +1,24 @@
-console.log(getname1)
-getname1();
-console.log(getname2);
-getname2();
-console.log(x1);
+// console.log(getname1)
+// getname1();
+// console.log(getname2);
+// getname2();
+// console.log(x1);
 
-var x1=10;
+// var x1=10;
 
-var getname1=()=>{
-    console.log("kartik1");
+// var getname1=()=>{
+//     console.log("kartik1");
 
-}
+// }
 
-var getname2=function(){
-    console.log("kartik2");
+// var getname2=function(){
+//     console.log("kartik2");
+// }
+
+getname();
+console.log(getname);
+console.log(x);
+var x=10;
+function getname(){
+    console.log("kartik");
 }
