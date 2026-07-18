385911a64217e1bfc335569e9d5ab8280ad5f9d0
Kartik Gupta <kg877253@gmail.com>
2026-07-18 19:01:03 +0530

    grreee5

--- Javascript/callback&promise/callbacks.js
+++ Javascript/callback&promise/callbacks.js
@@ -1,11 +1,20 @@
 console.log("helloooo")
 
+
+
+
+
 function greet(name, Callbackhai) {
   console.log("Hello " + name);
   Callbackhai();
 }
+
 function callbackkliye() {
   console.log("Callback executed!");
 }
+
 let nameofp = "Kartik";
+
+
+
 greet(nameofp, callbackkliye);
