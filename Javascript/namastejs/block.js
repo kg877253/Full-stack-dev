a5245b72f2db8bf12fa99d3dfb934ee8c44d897f
Kartik Gupta <kg877253@gmail.com>
2026-07-18 14:34:38 +0530

    shadow

--- Javascript/namastejs/block.js
+++ Javascript/namastejs/block.js
@@ -1,13 +1,20 @@
-let b=100;
-var a=1000;
-console.log(a);
-{
+// let b=100;
+// var a=1000;
+// console.log(a);
+// {
+//     var a=10;
+//     let b=20;
+//     const c=30;
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+// console.log(b);
+// console.log(a);//shadowing 
+
+let a =100;
+function c(){
     var a=10;
-    let b=20;
-    const c=30;
     console.log(a);
-    console.log(b);
-    console.log(c);
 }
-console.log(b);
-console.log(a);//shadowing 
+c();
