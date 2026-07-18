706a1c1144db8324308a787f537ea0bd514dd7c9
Kartik Gupta <kg877253@gmail.com>
2026-07-18 14:23:43 +0530

    block

---
+++ Javascript/namastejs/block.js
@@ -0,0 +1,13 @@
+let b=100;
+var a=1000;
+console.log(a);
+{
+    var a=10;
+    let b=20;
+    const c=30;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+console.log(b);
+console.log(a);
