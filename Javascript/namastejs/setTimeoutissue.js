ca5fc92fc6dffbf08dc7ae003d716a2895ba387c
Kartik Gupta <kg877253@gmail.com>
2026-07-19 14:53:54 +0530

    set

---
+++ Javascript/namastejs/setTimeoutissue.js
@@ -0,0 +1,16 @@
+console.log("start");
+
+setTimeout(function cb() {
+    console.log("callbACK");
+},5000);
+
+console.log("end");
+
+//million lines of code
+
+let startTime = new Date().getTime();
+let endTime=startTime;
+
+while(endTime<startTime+10000){
+    endTime=new Date().getTime();
+}
