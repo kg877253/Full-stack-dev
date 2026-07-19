3e742a6711f13ce2b34cbdeec6ce2619c535ca8b
Kartik Gupta <kg877253@gmail.com>
2026-07-19 22:02:08 +0530

    updated

--- Javascript/namastejs/setTimeoutissue.js
+++ Javascript/namastejs/setTimeoutissue.js
@@ -14,7 +14,28 @@ let endTime=startTime;
 while(endTime<startTime+10000){
     endTime=new Date().getTime();
 
-    
+
 }
 
-console.log("while loop ended");
+// console.log("while loop ended");
+
+// console.log("start");
+
+// setTimeout(function cb() {
+//     console.log("callbACK");
+// },5000);
+
+// console.log("end");
+
+// //million lines of code
+
+// let startTime = new Date().getTime();
+// let endTime=startTime;
+
+// while(endTime<startTime+10000){
+//     endTime=new Date().getTime();
+
+    
+// }
+
+// console.log("while loop ended");
