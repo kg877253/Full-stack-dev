beecdd4ebffd176d251ad8863d6cc609c6726036
Kartik Gupta <kg877253@gmail.com>
2026-07-23 16:23:37 +0530

    downloading songs and adding them to the project

--- SPOTIFY CLONE/index.js
+++ SPOTIFY CLONE/index.js
@@ -1,1 +1,29 @@
 console.log("Welcome to Spotify Clone!");
+
+
+async function getSongs() {
+    let a = await fetch("http://127.0.0.1:3000/SPOTIFY%20CLONE/songs/")
+    let response = await a.text();
+    // console.log(response);
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as=div.getElementsByTagName("a");
+    // console.log(as);
+    
+    let songs = [];
+
+    for(let i =0 ;i<as.length;i++){
+        const element = as[i];
+
+        if(element.href.endsWith("mp3")){
+            songs.push(element.href);
+        }
+    }
+    return songs;
+}
+
+async function main(){
+    let songss=await getSongs();
+    console.log(songss);
+}
+main();
