b5e569c0a3a9c603f11c767075f9ab33b6381087
Kartik Gupta <kg877253@gmail.com>
2026-07-29 17:47:40 +0530

    working with files in JS(BACKEND)/86video

--- JS(BACKEND)/86video/main.js
+++ JS(BACKEND)/86video/main.js
@@ -1,3 +1,16 @@
-console.log("kartik")
+
 const fs = require('fs');
+console.log("starting")
+//this is synchronous method ek hi line me execute hojayega aur agla line execute hoga
+// fs.writeFileSync('kartik.txt', 'hello kartik');
+//this is asynchronous method schedule hojayega aur bad me chlega (preffered method)
+fs.writeFile('kartik2.txt', 'hello kartik2',()=>{
+    console.log("file created")
+    fs.readFile('kartik2.txt', (err, data)=>{
+        console.log(err, data.toString());
+    });
+});
+
+console.log("ending");
+
 
