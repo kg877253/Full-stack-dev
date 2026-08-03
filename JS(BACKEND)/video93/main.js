328dac27fc1e7fcd0ef2ffba8f8a4ab1073c19e2
Kartik Gupta <kg877253@gmail.com>
2026-08-03 19:17:10 +0530

    video 93- learned how to use ejs template engine in node js and express js. we created a blog website using ejs template engine. we created a navbar, home page and blog post page using ejs template engine. we also learned how to use partials in ejs template engine.

---
+++ JS(BACKEND)/video93/main.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+//bhut types k view engine hote h like ejs, pug, handlebars etc
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    let sitename= "adidas";
+    let searchbar = "Search now";
+    res.render('index', {sitename: sitename, searchbar: searchbar});
+});
+
+app.get('/blog/:slug', (req, res) => {
+    let blogtitle = "adidas shoes";
+    let blogcontent="good brand & shoes"
+    res.render('blogpost', { blogtitle: blogtitle, blogcontent: blogcontent });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
