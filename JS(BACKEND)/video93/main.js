b2a7730b6a4233a1912a039b045dec4e3d02c7be
Kartik Gupta <kg877253@gmail.com>
2026-08-03 19:17:50 +0530

    revise this as much as possible

--- JS(BACKEND)/video93/main.js
+++ JS(BACKEND)/video93/main.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
 //bhut types k view engine hote h like ejs, pug, handlebars etc
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    let sitename= "adidas";
+    let sitename = "adidas";
     let searchbar = "Search now";
-    res.render('index', {sitename: sitename, searchbar: searchbar});
+    res.render('index', { sitename: sitename, searchbar: searchbar });
 });
 
 app.get('/blog/:slug', (req, res) => {
     let blogtitle = "adidas shoes";
-    let blogcontent="good brand & shoes"
+    let blogcontent = "good brand & shoes"
     res.render('blogpost', { blogtitle: blogtitle, blogcontent: blogcontent });
 });
 
