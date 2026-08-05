f2e357fa5786285d4abf60530f7a0536414a45df
Kartik Gupta <kg877253@gmail.com>
2026-08-05 21:46:58 +0530

    learning mongodb now

---
+++ JS(BACKEND)/mongodb full/playground-1.mongodb.js
@@ -0,0 +1,51 @@
+
+// Select the database to use.
+use('mongodbVSCodePlaygroundDB');
+
+// Insert a few documents into the courses collection.
+db.getCollection('courses').insertMany([
+  {
+    name: 'python',
+    price: NumberInt('150000')
+  },
+  {
+    name: 'javascript',
+    price: NumberInt('180000')
+  },
+  {
+    name: 'react',
+    price: NumberInt('220000')
+  },
+  {
+    name: 'nodejs',
+    price: NumberInt('190000')
+  },
+  {
+    name: 'mongodb',
+    price: NumberInt('160000')
+  },
+  {
+    name: 'express',
+    price: NumberInt('120000')
+  },
+  {
+    name: 'typescript',
+    price: NumberInt('210000')
+  },
+  {
+    name: 'c++',
+    price: NumberInt('17000000')
+  },
+  {
+    name: 'sql',
+    price: NumberInt('140000')
+  },
+  {
+    name: 'docker',
+    price: NumberInt('230000')
+  }
+]);
+
+
+// Print a message to the output window.
+console.log(`data inserted successfully`);
