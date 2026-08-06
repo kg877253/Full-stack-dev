da168db3a861c89a084fdaca07c6cb96cfb45033
Kartik Gupta <kg877253@gmail.com>
2026-08-06 16:53:46 +0530

    used mongoose and learned about schema and model. created a todo schema and exported the model.

--- JS(BACKEND)/mongoose/main.js
+++ JS(BACKEND)/mongoose/main.js
@@ -2,24 +2,24 @@ import mongoose from "mongoose";
 import express from "express";
 import { Todo } from "./modules/todo.js";
 
-let conn= await mongoose.connect("mongodb://localhost:27017/todo");
+await mongoose.connect("mongodb://localhost:27017/todo");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
     const todo1 = new Todo({
         title: "Buy groceries",
         description: "Milk, Bread",
         completed: false,
         days: 3
     });
-    todo1.save();
+    await todo1.save();
     res.send("Todo added!");
 });
 
 app.get("/a", async(req, res) => {
-    let todo= await Todo.findMany({completed: false});
-    res.json({title: todo.title, description: todo.description, completed: todo.completed, days: todo.days});
+    const todos = await Todo.find({completed: false});
+    res.json(todos);
 });
 
 
