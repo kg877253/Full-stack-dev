b984ce4f8dbe6889501847f74e2a82167bec8d53
Kartik Gupta <kg877253@gmail.com>
2026-08-14 15:04:16 +0530

    created cards using components and added css for cards, navbar and footer

--- React/Components/First-app/src/App.jsx
+++ React/Components/First-app/src/App.jsx
@@ -6,14 +6,14 @@ function App() {
 
   return (
     <>
-     <Navbar/>
-     <Footer/>
-     <div className="cards">
-      <Card title="Card 1" description="This is the description for Card 1." />
-      <Card title="Card 2" description="This is the description for Card 2." />
-      <Card title="Card 3" description="This is the description for Card 3." />
-      <Card title="Card 4" description="This is the description for Card 4." />
-     </div>
+      <Navbar />
+      <Footer />
+      <div className="cards">
+        <Card title="Card 1" description="This is the description for Card 1." />
+        <Card title="Card 2" description="This is the description for Card 2." />
+        <Card title="Card 3" description="This is the description for Card 3." />
+        <Card title="Card 4" description="This is the description for Card 4." />
+      </div>
     </>
   )
 }
--- React/Components/First-app/src/components/Card.jsx
+++ React/Components/First-app/src/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = (props) => {
     return (
         <div className='card'>
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq2VAKb-ju088wxKToxQ3ONysuWGENhmo-S8QBkR5Akg&s=10" alt="" width={333} height={200}
-            style={{border: "2px solid black"}} />
+                style={{ border: "2px solid black" }} />
             <h2>{props.title}</h2>
             <p>{props.description}</p>
         </div>
