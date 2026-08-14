841c1a2d6e4806d2dbd23bea4e92b3c75cb15278
Kartik Gupta <kg877253@gmail.com>
2026-08-14 15:03:29 +0530

    created navbar and footer using Components

--- React/Components/First-app/src/App.jsx
+++ React/Components/First-app/src/App.jsx
@@ -1,10 +1,19 @@
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import Card from "./components/Card"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <Navbar/>
+     <Footer/>
+     <div className="cards">
+      <Card title="Card 1" description="This is the description for Card 1." />
+      <Card title="Card 2" description="This is the description for Card 2." />
+      <Card title="Card 3" description="This is the description for Card 3." />
+      <Card title="Card 4" description="This is the description for Card 4." />
+     </div>
     </>
   )
 }
---
+++ React/Components/First-app/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer'>
+      Kartik gupta || www.kartikgupta.com | &copy; 2024
+    </div>
+  )
+}
+
+export default Footer
