5902b4f23eb2a1250a936727c6489f7e626d0c0a
Kartik Gupta <kg877253@gmail.com>
2026-08-18 13:24:52 +0530

    revision from 85th video to 113th video

--- JS(BACKEND)/84video/practice.js
+++ JS(BACKEND)/84video/practice.js
@@ -1,15 +1,17 @@
+//ex of named import 
 // import {a, b, c} from './mymodule.js';
 
 // console.log(a); // Output: 1
 // console.log(b); // Output: 2
 // console.log(c); // Output: 3
 
+//ex of default import
 import kartik from './mymodule.js';
 console.log(kartik.name); // Output: John
 
 
 
-
+//ex of common js module
 // const a = require('./mymodule2.js');
 // console.log(a);
 
