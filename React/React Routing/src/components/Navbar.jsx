f9e6037110f583f7ea2bee0c3f9e9e790e3e54fc
Kartik Gupta <kg877253@gmail.com>
2026-08-20 16:54:50 +0530

    when we write /user/kartik it will display the user:kartik in the page

--- React/React Routing/src/components/Navbar.jsx
+++ React/React Routing/src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 const Navbar = () => {
-  return (
-      <nav>
-        <ul>
-          <NavLink className={(e)=>{return e.isActive?"red":"blue"}} to="/"> <li>Home</li> </NavLink>
-          <NavLink className={(e)=>{return e.isActive?"red":"blue"}} to="/about"> <li>About</li> </NavLink>
-          <NavLink className={(e)=>{return e.isActive?"red":"blue"}} to="/login"> <li>Login</li> </NavLink>
-        </ul>
-      </nav>
-  )
+    return (
+        <nav>
+            <ul>
+                <NavLink className={(e) => { return e.isActive ? "red" : "blue" }} to="/"> <li>Home</li> </NavLink>
+                <NavLink className={(e) => { return e.isActive ? "red" : "blue" }} to="/about"> <li>About</li> </NavLink>
+                <NavLink className={(e) => { return e.isActive ? "red" : "blue" }} to="/login"> <li>Login</li> </NavLink>
+            </ul>
+        </nav>
+    )
 }
 
 export default Navbar
