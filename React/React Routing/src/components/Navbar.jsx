cba1be39f39494594c912e37a77e1765c64dc767
Kartik Gupta <kg877253@gmail.com>
2026-08-20 16:53:34 +0530

    applying css and using navlink

--- React/React Routing/src/components/Navbar.jsx
+++ React/React Routing/src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
       <nav>
         <ul>
-          <Link to="/"> <li>Home</li> </Link>
-          <Link to="/about"> <li>About</li> </Link>
-          <Link to="/login"> <li>Login</li> </Link>
+          <NavLink className={(e)=>{return e.isActive?"red":"blue"}} to="/"> <li>Home</li> </NavLink>
+          <NavLink className={(e)=>{return e.isActive?"red":"blue"}} to="/about"> <li>About</li> </NavLink>
+          <NavLink className={(e)=>{return e.isActive?"red":"blue"}} to="/login"> <li>Login</li> </NavLink>
         </ul>
       </nav>
   )
