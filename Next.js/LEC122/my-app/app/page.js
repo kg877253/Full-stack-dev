3e27ca1a0a19a9149385526f6f6685e525297870
Kartik Gupta <kg877253@gmail.com>
2026-08-28 16:38:36 +0530

    changes

--- Next.js/LEC122/my-app/app/page.js
+++ Next.js/LEC122/my-app/app/page.js
@@ -6,11 +6,11 @@ export default function Home() {
   // const [count, setcount] = useState(0);
   console.log("counddddt");
   const a = fs.readFile(".gitignore");
-  a.then((e)=> console.log(e.toString()));
+  a.then((e) => console.log(e.toString()));
 
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <h1 className="text-3xl border-amber-50 border-2 p-3 m-2">Welcome to the Home Page <span className="border-4 p-1 border-cyan-700">
         {/* {count} */}</span></h1>
       {/* <button className="text-blue-300 border-2 rounded-2xl p-2 m-2 ml-90 cursor-pointer" onClick={()=> setcount(count+1)}>a +</button> */}
