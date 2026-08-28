5b94b7353dbef3172f3f87f1480e3a8e60691c16
Kartik Gupta <kg877253@gmail.com>
2026-08-28 14:55:20 +0530

    we can route the pages very simply by creating a new folder in the app directory and then creating a page.js file inside that folder. The name of the folder will be the name of the route. For example, if we create a folder called blog and then create a page.js file inside that folder, we can access that page by going to /blog in the browser.

--- Next.js/firstnextjs/app/page.js
+++ Next.js/firstnextjs/app/page.js
@@ -2,8 +2,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <div className="bg-red-500">
-      I am don
-    </div></>
+      <div className="bg-red-800">
+        I am don
+      </div>
+    </>
   );
 }
