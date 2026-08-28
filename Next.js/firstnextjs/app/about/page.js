c27719b02b72b0222c8f85f1e9b946ac5f39c95c
Kartik Gupta <kg877253@gmail.com>
2026-08-28 14:53:33 +0530

    Learning Next.js

---
+++ Next.js/firstnextjs/app/about/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='text-xl font-bold'>About Us</h1>
+    </div>
+  )
+}
+
+export default page
---
+++ Next.js/firstnextjs/app/blog/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const pages = () => {
+  return (
+    <>
+    <br />
+    <div className='text-3xl font-bold'>
+      Iam blog
+    </div>
+    </>
+  )
+}
+
+export default pages
