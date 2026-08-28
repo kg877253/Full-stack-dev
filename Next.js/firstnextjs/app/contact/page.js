db074f54d01d6b6cc16ae4c413fc2711682e9beb
Kartik Gupta <kg877253@gmail.com>
2026-08-28 14:54:55 +0530

    changes

--- Next.js/firstnextjs/app/contact/page.js
+++ Next.js/firstnextjs/app/contact/page.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const page = () => {
-  return (
-    <div>
-      <h1 className='text-xl font-bold'>Contact Us</h1>
-    </div>
-  )
+    return (
+        <div>
+            <h1 className='text-2xl font-bold'>Contact Us</h1>
+        </div>
+    )
 }
 
 export default page
