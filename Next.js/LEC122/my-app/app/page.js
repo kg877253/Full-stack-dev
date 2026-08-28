606e0779238639f608351201e0d1ba81e2a670b6
Kartik Gupta <kg877253@gmail.com>
2026-08-28 16:35:54 +0530

    server side and client side components are different in nextjs. server side components are rendered on the server and sent to the client as HTML, while client side components are rendered on the client using JavaScript. server side components can fetch data from a database or an API, while client side components can use state and effects to manage user interactions. server side components can be used for SEO and performance, while client side components can be used for dynamic and interactive features.

--- Next.js/LEC122/my-app/app/page.js
+++ Next.js/LEC122/my-app/app/page.js
@@ -1,12 +1,18 @@
-"use client"
-import { useState, useEffect } from "react";
+// "use client"
+import fs from "fs/promises";
+// import { useState, useEffect } from "react";
 export default function Home() {
-  const [count, setcount] = useState(0);
+  // const [count, setcount] = useState(0);
+  console.log("counddddt");
+  const a = fs.readFile(".gitignore");
+  a.then((e)=> console.log(e.toString()));
+
   return (
     <div>
-      <h1 className="text-3xl border-amber-50 border-2 p-3 m-2">Welcome to the Home Page <span className="border-4 p-1 border-cyan-700">{count}</span></h1>
-      <button className="text-blue-300 border-2 rounded-2xl p-2 m-2 ml-90 cursor-pointer" onClick={()=> setcount(count+1)}>a +</button>
-      <button className="text-blue-300 border-2 rounded-2xl p-2 m-2 ml-20 cursor-pointer" onClick={()=> setcount(count-1)}>m -</button>
+      <h1 className="text-3xl border-amber-50 border-2 p-3 m-2">Welcome to the Home Page <span className="border-4 p-1 border-cyan-700">
+        {/* {count} */}</span></h1>
+      {/* <button className="text-blue-300 border-2 rounded-2xl p-2 m-2 ml-90 cursor-pointer" onClick={()=> setcount(count+1)}>a +</button> */}
+      {/* <button className="text-blue-300 border-2 rounded-2xl p-2 m-2 ml-20 cursor-pointer" onClick={()=> setcount(count-1)}>m -</button> */}
     </div>
   );
 }
