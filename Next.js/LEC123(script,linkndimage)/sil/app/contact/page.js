991720ad497bd40339c143438810e56e646db5b4
Kartik Gupta <kg877253@gmail.com>
2026-08-29 18:28:30 +0530

    learning how to use script tag in next.js

--- Next.js/LEC123(script,linkndimage)/sil/app/contact/page.js
+++ Next.js/LEC123(script,linkndimage)/sil/app/contact/page.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Script from 'next/script'
 
 const contact = () => {
   return (
     <div>
       contactttttttttt
+      {/* <Script>
+        {`alert('hello')`}
+      </Script> */}
+      <Script src="https://example.com/script.js" />
     </div>
   )
 }
