7bcd2c0b8c165a677022ec723ac94fd71e95f373
Kartik Gupta <kg877253@gmail.com>
2026-08-30 14:41:00 +0530

    Learning how to create an API in Next.js. This commit includes the initial setup of a Next.js application with a simple API route that returns a message. The frontend has been updated to fetch data from this API and display it, with loading state management implemented for better user experience.

---
+++ Next.js/LEC124(creating API)/my-app/app/page.js
@@ -0,0 +1,34 @@
+"use client"
+import Image from "next/image";
+import { useState } from "react";
+export default function Home() {
+  const [data, setData] = useState("");
+  const [loading, setLoading] = useState(false)
+  const handleclick= async()=>{
+    let data = {
+      name : "sachin",
+      age : 23
+    }
+    let res= await fetch("/api/add",{method: "POST", headers: {"Content-Type": "application/json"}, body: JSON.stringify(data)})
+    let a = await res.json()
+    console.log(a)
+  }
+  const handleclick2= async()=>{
+    setLoading(true)
+    let res= await fetch("/api/add",{method: "GET"})
+    let a = await res.json()
+    console.log(a)
+    setLoading(false)
+    setData(a.message)
+  }
+  return (
+    <div>
+      <h1>Welcome to Next.js!</h1>
+      {/* <button onClick={handleclick}>Click me</button> */}
+      <button onClick={handleclick2} disabled={loading}>
+      { loading ? "Loading..." : "Click me"}
+      </button>
+      {  data && <p>{data}</p>}
+    </div>
+  );
+}
