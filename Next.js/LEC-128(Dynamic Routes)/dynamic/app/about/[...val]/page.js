641936dcab4b2efeb6752c17ad38d5647fca0c4b
Kartik Gupta <kg877253@gmail.com>
2026-08-31 16:04:01 +0530

    Learned about Error handling and how to display error messages in Next.js. Implemented a custom error component that logs errors to the console and provides a retry option for users. This enhances the user experience by allowing them to recover from errors without leaving the page.

--- Next.js/LEC-128(Dynamic Routes)/dynamic/app/about/[...val]/page.js
+++ Next.js/LEC-128(Dynamic Routes)/dynamic/app/about/[...val]/page.js
@@ -1,6 +1,14 @@
-export default function Page({ params }) {
-  const { val } = params;
-  const slug = Array.isArray(val) ? val.join('/') : val;
+export default async function Page({ params }) {
 
-  return <div>this is a about test params slug: {slug}</div>;
+    //when we want more than one parameter we can use [...val] in the file name and then we can get the values in the params object as an array
+
+    //but when we want only one parameter we can use [val] in the file name and then we can get the value in the params object as a string 
+
+    const { val } = await params;
+    //const slug = Array.isArray(val) ? val.join('/') : val;
+
+    console.log(val)
+    
+    const value = val.join('/')
+    return <div>this is a about test params slug: {value}</div>;
 }
