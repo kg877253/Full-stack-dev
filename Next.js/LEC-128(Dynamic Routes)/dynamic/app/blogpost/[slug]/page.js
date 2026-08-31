5efd08ae9290c68ced3c2bc4fa389c49d7380fef
Kartik Gupta <kg877253@gmail.com>
2026-08-31 16:04:45 +0530

    if we want only 1 param then use [slug] and if we want multiple params then use [...slug] in the file name.

--- Next.js/LEC-128(Dynamic Routes)/dynamic/app/blogpost/[slug]/page.js
+++ Next.js/LEC-128(Dynamic Routes)/dynamic/app/blogpost/[slug]/page.js
@@ -1,14 +1,14 @@
 export default async function Page({ params }) {
-  const { slug } = await params
-  let lang=["python","javascript","java","cpp","c#","ruby","php","go","swift","kotlin","typescript"]
-  //we can do this also 
-//   return <div>My Post: {slug}</div>
+    const { slug } = await params
+    let lang = ["python", "javascript", "java", "cpp", "c#", "ruby", "php", "go", "swift", "kotlin", "typescript"]
+    //we can do this also 
+    //   return <div>My Post: {slug}</div>
 
-//or this if we have to give blog only on some particular languages or things
-  if(lang.includes(slug)){
-     return <div>My Post: {slug}</div>
-  }
-  else{
-    return <div className="bg-amber-100 text-black h-[100vh] w-full text-7xl text-center content-center font-bold">Post not found</div>
-  }
+    //or this if we have to give blog only on some particular languages or things
+    if (lang.includes(slug)) {
+        return <div>My Post: {slug}</div>
+    }
+    else {
+        return <div className="bg-amber-100 text-black h-[100vh] w-full text-7xl text-center content-center font-bold">Post not found</div>
+    }
 }
