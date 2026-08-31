89b4ef4d1d04bd130da0e8c353d4cb145417dac2
Kartik Gupta <kg877253@gmail.com>
2026-08-31 15:57:23 +0530

    Learning how to make 404 custom page

--- Next.js/LEC-128(Dynamic Routes)/dynamic/app/about/[...val]/page.js
+++ Next.js/LEC-128(Dynamic Routes)/dynamic/app/about/[...val]/page.js
@@ -1,7 +1,6 @@
-export default async function Page({ params }) {
-    
-    const { slug } = await params
-    let a = await params
-    console.log(a)
-    return <div>this is a about test params slug: {slug} </div>
+export default function Page({ params }) {
+  const { val } = params;
+  const slug = Array.isArray(val) ? val.join('/') : val;
+
+  return <div>this is a about test params slug: {slug}</div>;
 }
--- Next.js/LEC-128(Dynamic Routes)/dynamic/app/blogpost/[slug]/page.js
+++ Next.js/LEC-128(Dynamic Routes)/dynamic/app/blogpost/[slug]/page.js
@@ -1,14 +1,10 @@
-export default async function Page({ params }) {
-  const { slug } = await params
-  let lang=["python","javascript","java","cpp","c#","ruby","php","go","swift","kotlin","typescript"]
-  //we can do this also 
-//   return <div>My Post: {slug}</div>
+export default function Page({ params }) {
+  const { slug } = params;
+  let lang = ["python", "javascript", "java", "cpp", "c#", "ruby", "php", "go", "swift", "kotlin", "typescript"];
 
-//or this if we have to give blog only on some particular languages or things
-  if(lang.includes(slug)){
-     return <div>My Post: {slug}</div>
+  if (lang.includes(slug)) {
+    return <div>My Post: {slug}</div>;
   }
-  else{
-    return <div className="bg-amber-100 text-black h-[100vh] w-full text-7xl text-center content-center font-bold">Post not found</div>
-  }
+
+  return <div className="bg-amber-100 text-black h-[100vh] w-full text-7xl text-center content-center font-bold">Post not found</div>;
 }
