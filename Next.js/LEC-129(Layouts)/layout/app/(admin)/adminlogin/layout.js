7b3441160b837ee5f734909e51d8439508ab7a21
Kartik Gupta <kg877253@gmail.com>
2026-09-02 21:02:52 +0530

    hn

--- Next.js/LEC-129(Layouts)/layout/app/(admin)/adminlogin/layout.js
+++ Next.js/LEC-129(Layouts)/layout/app/(admin)/adminlogin/layout.js
@@ -10,10 +10,8 @@ export default function AdminloginLayout({ children }) {
   return (
     <html lang="en" className={`h-full antialiased`}>
       <body className="min-h-full flex flex-col">
-        
         <span>login navbar</span>
         {children}
-        
       </body>
     </html>
   );
