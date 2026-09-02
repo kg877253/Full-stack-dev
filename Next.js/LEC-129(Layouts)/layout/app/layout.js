a49adef07aaa016079e7dcc52751d1aebaf20e8d
Kartik Gupta <kg877253@gmail.com>
2026-09-02 21:02:18 +0530

    changes

--- Next.js/LEC-129(Layouts)/layout/app/layout.js
+++ Next.js/LEC-129(Layouts)/layout/app/layout.js
@@ -12,9 +12,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`h-full antialiased`}>
       <body className="min-h-full flex flex-col">
-        <Navbar/>
+        <Navbar />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
