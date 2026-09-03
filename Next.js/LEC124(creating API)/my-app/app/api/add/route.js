7a8b34249eeb1982f30f9fe0fa27c967149f5270
Kartik Gupta <kg877253@gmail.com>
2026-09-03 21:46:18 +0530

    voice

--- Next.js/LEC124(creating API)/my-app/app/api/add/route.js
+++ Next.js/LEC124(creating API)/my-app/app/api/add/route.js
@@ -7,6 +7,7 @@ export async function GET() {
 
 export async function POST(request) {
     const data = await request.json();
+    
     console.log(data);
     return NextResponse.json({success:true , data })
 }
