39d7ef07184f795f0d81e5dd1a03cbbc53b19baf
Kartik Gupta <kg877253@gmail.com>
2026-09-03 21:46:06 +0530

    name

--- Next.js/LEC124(creating API)/my-app/app/api/add/route.js
+++ Next.js/LEC124(creating API)/my-app/app/api/add/route.js
@@ -4,6 +4,7 @@ export async function GET() {
   return NextResponse.json({ message: 'Hekkiiiiio worldd' })
 }
 
+
 export async function POST(request) {
     const data = await request.json();
     console.log(data);
