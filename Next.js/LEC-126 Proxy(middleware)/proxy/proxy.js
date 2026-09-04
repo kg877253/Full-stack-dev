a975bfaa5f126ccb9bb968cafcca590291a37fe6
Kartik Gupta <kg877253@gmail.com>
2026-09-04 15:42:00 +0530

    proxy redirect and rewrite

--- Next.js/LEC-126 Proxy(middleware)/proxy/proxy.js
+++ Next.js/LEC-126 Proxy(middleware)/proxy/proxy.js
@@ -10,38 +10,38 @@
 // }
 
 
-// import { NextResponse } from 'next/server'
- 
-// export function proxy(request) {
-//   if (request.nextUrl.pathname.startsWith('/about')) {
-//     return NextResponse.rewrite(new URL('/about-2', request.url))
-//   }
- 
-//   if (request.nextUrl.pathname.startsWith('/dashboard')) {
-//     return NextResponse.redirect(new URL('/dashboard/user', request.url))
-//   }
-//   if( request.nextUrl.pathname.startsWith('/home')) {
-//     return NextResponse.rewrite(new URL('/home', request.url))
-//   }
-// }
-
-
 import { NextResponse } from 'next/server'
  
 export function proxy(request) {
-  // Clone the request headers and set a new header `x-hello-from-proxy1`
-  const requestHeaders = new Headers(request.headers)
-  requestHeaders.set('x-hello-from-proxy1', 'hello')
+  if (request.nextUrl.pathname.startsWith('/about')) {
+    return NextResponse.rewrite(new URL('/about-2', request.url))
+  }
  
-  // You can also set request headers in NextResponse.next
-  const response = NextResponse.next({
-    request: {
-      // New request headers
-      headers: requestHeaders,
-    },
-  })
- 
-  // Set a new response header `x-hello-from-proxy2`
-  response.headers.set('x-hello-from-proxy2', 'hello')
-  return response
+  if (request.nextUrl.pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/dashboard/user', request.url))
+  }
+  if( request.nextUrl.pathname.startsWith('/home')) {
+    return NextResponse.rewrite(new URL('/home', request.url))
+  }
 }
+
+
+// import { NextResponse } from 'next/server'
+ 
+// export function proxy(request) {
+//   // Clone the request headers and set a new header `x-hello-from-proxy1`
+//   const requestHeaders = new Headers(request.headers)
+//   requestHeaders.set('x-hello-from-proxy1', 'hello')
+ 
+//   // You can also set request headers in NextResponse.next
+//   const response = NextResponse.next({
+//     request: {
+//       // New request headers
+//       headers: requestHeaders,
+//     },
+//   })
+ 
+//   // Set a new response header `x-hello-from-proxy2`
+//   response.headers.set('x-hello-from-proxy2', 'hello')
+//   return response
+// }
