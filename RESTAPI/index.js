58069fc85127de1b0cce15d1cc331d1cede4a960
Kartik Gupta <kg877253@gmail.com>
2026-09-05 14:48:09 +0530

    GET AND POST Req in postman

--- RESTAPI/index.js
+++ RESTAPI/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const fs = require('fs')
 const users = require('./MOCK_DATA.json')
 const app = express()
 const port = 3000
+
+//Middleware
+app.use(express.urlencoded({ extended: false })) //Parse URL-encoded bodies (as sent by HTML forms)
 
 //ROUTES
 app.get('/users', (req, res) => {
@@ -29,6 +33,13 @@ app.route('/api/users/:id').get((req, res) => {
 }).delete((req, res) => {
     //Delete user
     return res.json({ status: 'pending deletion' })
+})
+
+app.post('/api/users', (req, res) => {
+    const bd= req.body
+    users.push({id: users.length + 1, ...bd})
+    fs.writeFileSync('./MOCK_DATA.json', JSON.stringify(users))
+    return res.json({ status: 'success', id: users.length})
 })
 
 app.listen(port, () => {
