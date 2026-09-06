811a1d5b3e50f3bc862ef70e3b0b2568c9689003
Kartik Gupta <kg877253@gmail.com>
2026-09-06 18:22:27 +0530

    functions to gen short url and to get the long url from short url

--- URL-Shortner/controller/url.js
+++ URL-Shortner/controller/url.js
@@ -1,14 +1,14 @@
-const {nanoid} = require('nanoid');
+const { nanoid } = require('nanoid');
 const URL = require('../models/url');
 
 //Function to generate a new short URL
-async function gennewshorturl(req , res){
+async function gennewshorturl(req, res) {
     const shortId = nanoid(8);
     const body = req.body;
-    if(await URL.findOne({redirectUrl: body.url})){
+    if (await URL.findOne({ redirectUrl: body.url })) {
         return res.status(400).json({ error: 'URL already exists' });
     }
-    if(!body.url) {
+    if (!body.url) {
         return res.status(400).json({ error: 'URL is required' });
     }
     await URL.create({
@@ -21,7 +21,7 @@ async function gennewshorturl(req , res){
 }
 
 //Function to get analytics for a short URL
-async function getanalytics(req , res){
+async function getanalytics(req, res) {
     const shortId = req.params.shortId;
     const entry = await URL.findOne({ shortId: shortId });
     if (!entry) {
@@ -31,7 +31,7 @@ async function getanalytics(req , res){
 }
 
 //Function to redirect to the original URL and record the visit
-async function gotoshorturl(req, res){
+async function gotoshorturl(req, res) {
 
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate({ shortId: shortId }, { $push: { visithistory: { timestamp: new Date() } } }, { returnDocument: 'after' });
@@ -42,4 +42,4 @@ async function gotoshorturl(req, res){
     return res.redirect(301, entry.redirectUrl);
 }
 
-module.exports = {gennewshorturl, getanalytics, gotoshorturl};
+module.exports = { gennewshorturl, getanalytics, gotoshorturl };
