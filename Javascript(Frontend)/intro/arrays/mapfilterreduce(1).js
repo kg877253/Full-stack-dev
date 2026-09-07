1c1f078f84d6e8b118bcb766f3b6382ea1b18f9f
Kartik Gupta <kg877253@gmail.com>
2026-09-07 21:45:08 +0530

    chnges

--- Javascript(Frontend)/intro/arrays/mapfilterreduce(1).js
+++ Javascript(Frontend)/intro/arrays/mapfilterreduce(1).js
@@ -1,5 +1,6 @@
 //ek naya array create karte hai
 //map method returns a new array with the results of calling a provided function on every element in the calling array.
+
 let a = [10, 20, 30, 40, 50]
 let b = a.map((value) => {
     return value * 2
