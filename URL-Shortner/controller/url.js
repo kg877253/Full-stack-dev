ca16938ce351d6f1027ea227d81ec45390c21f21
Kartik Gupta <kg877253@gmail.com>
2026-09-08 18:54:04 +0530

    learning server side rendering with ejs and express.

--- URL-Shortner/controller/url.js
+++ URL-Shortner/controller/url.js
@@ -20,6 +20,18 @@ async function gennewshorturl(req, res) {
     return res.status(201).json({ shortId: shortId });
 }
 
+//Function to redirect to the original URL and record the visit
+async function gotoshorturl(req, res) {
+
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate({ shortId: shortId }, { $push: { visithistory: { timestamp: new Date() } } }, { returnDocument: 'after' });
+    if (!entry) {
+        return res.status(404).json({ error: 'Short URL not founds' });
+    }
+    // is se original URL pe redirect karenge
+    return res.redirect(301, entry.redirectUrl);
+}
+
 //Function to get analytics for a short URL
 async function getanalytics(req, res) {
     const shortId = req.params.shortId;
@@ -30,16 +42,5 @@ async function getanalytics(req, res) {
     return res.status(200).json({ totalclicks: entry.visithistory.length, visithistory: entry.visithistory });
 }
 
-//Function to redirect to the original URL and record the visit
-async function gotoshorturl(req, res) {
-
-    const shortId = req.params.shortId;
-    const entry = await URL.findOneAndUpdate({ shortId: shortId }, { $push: { visithistory: { timestamp: new Date() } } }, { returnDocument: 'after' });
-    if (!entry) {
-        return res.status(404).json({ error: 'Short URL not found' });
-    }
-    // is se original URL pe redirect karenge
-    return res.redirect(301, entry.redirectUrl);
-}
 
 module.exports = { gennewshorturl, getanalytics, gotoshorturl };
--- URL-Shortner/index.js
+++ URL-Shortner/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+const URL = require('./models/url');
 const urlRoutes = require('./routes/url');
 
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
 app.use(express.json());
 
 app.use('/', urlRoutes);
@@ -15,6 +18,10 @@ mongoose.connect('mongodb://localhost:27017/shorturl').then(() => {
   console.error('Error connecting to MongoDB:', err);
 });
 
+app.get('/tot/allid', async(req, res) => {
+  const allurls= await URL.find({});
+  res.render('render',{url: allurls});
+});
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
