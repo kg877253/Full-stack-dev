62b108d5ce2a40461039b9156dc8d1b0abecccaa
Kartik Gupta <kg877253@gmail.com>
2026-09-09 21:06:12 +0530

    scxhema

--- URL-Shortner/models/url.js
+++ URL-Shortner/models/url.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const urlSchema = new mongoose.Schema({
+
     shortId: {
         type: String,
         required: true,
@@ -17,6 +18,7 @@ const urlSchema = new mongoose.Schema({
         }
     }],
 },
+
     { timestamps: true }
 );
 
