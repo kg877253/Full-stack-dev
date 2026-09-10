372e6abe6962617640afcf4e2bbbc64dd4420ab1
Kartik Gupta <kg877253@gmail.com>
2026-09-10 19:27:27 +0530

    enabling stateless authentication using jwt instead of stateful authentication using sessionid

--- URL-Shortner/controller/user.js
+++ URL-Shortner/controller/user.js
@@ -1,9 +1,9 @@
 const User = require('../models/user');
-const {v4: uuidv4} = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 const { setuser, getuser } = require('../service/auth');
 
 async function handleUserSignup(req, res) {
-    const {name, email, password} = req.body;
+    const { name, email, password } = req.body;
     await User.create({
         name: name,
         email: email,
@@ -28,12 +28,12 @@ async function handleUserSignup(req, res) {
 // }
 
 async function handleUserLogin(req, res) {
-    const { email, password} = req.body;
-    const user = await User.findOne({email: email,password: password });
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email, password: password });
 
-    if (!user){
+    if (!user) {
         return res.render('signup', { error: 'Email does not exist' });
-    } 
+    }
 
     const token = setuser(user);
     res.cookie('token', token, { httpOnly: true });
