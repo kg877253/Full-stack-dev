6fd3d68cea9f2ec9bc26fd5c9970dedf13592c5e
Kartik Gupta <kg877253@gmail.com>
2026-09-12 20:44:12 +0530

    Made button of deleting the password and also added uuid

--- PASSMAN/src/components/Manager.jsx
+++ PASSMAN/src/components/Manager.jsx
@@ -1,10 +1,12 @@
 import React, { useRef, useState, useEffect } from 'react'
+import { ToastContainer, toast, Bounce, Slide, Zoom , Flip } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { v4 as uuidv4 } from 'uuid';
 
 const Manager = () => {
 
   const ref = useRef()
   const passwordRef = useRef()
-  const gifref = useRef()
   const [form, setform] = useState({ site: "", username: "", password: "" })
   const [passwordarray, setpasswordarray] = useState([])
 
@@ -27,18 +29,51 @@ const Manager = () => {
   }
 
   const savepassword = () => {
-    setpasswordarray([...passwordarray, form])
-    localStorage.setItem("passwords", JSON.stringify([...passwordarray, form]))
+    setpasswordarray([...passwordarray, {...form, id: uuidv4() }])
+    localStorage.setItem("passwords", JSON.stringify([...passwordarray, {...form, id: uuidv4() }]))
     console.log([...passwordarray, form])
+  }
+
+  const deletePassword = (id) => {
+    const updatedArray = passwordarray.filter((item) => item.id !== id)
+    setpasswordarray(updatedArray)
+    localStorage.setItem("passwords", JSON.stringify(updatedArray))
+  }
+  
+  const editPassword = (id) => {
+    const passwordToEdit = passwordarray.find((item) => item.id === id);
   }
 
   const savechange = (e) => {
     setform({ ...form, [e.target.name]: e.target.value })
   }
 
+  const copytext = (text) => {
+    toast("Text copied to clipboard!", {
+      position: "bottom-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+      transition: Bounce,
+    });
+    navigator.clipboard.writeText(text)
+  }
 
   return (
     <>
+      <ToastContainer position="bottom-center" autoClose={5000} hideProgressBar={false} newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Bounce}
+      />
       <div className='absolute inset-0 -z-10 h-full w-full bg-[#1e1e1e] bg-[linear-gradient(to_right,#8080801a_1px,transparent_1px),linear-gradient(to_bottom,#8080801a_1px,transparent_1px)] bg-[size:14px_24px]'></div>
 
       <div className='text-white'>
@@ -70,25 +105,45 @@ const Manager = () => {
         <div>
           <div className='text-center text-3xl font-extrabold mt-1 underline decoration-dashed decoration-green-200'>Your Passwords</div>
           {passwordarray.length === 0 && <h1 className='text-center text-2xl mt-10'>No Passwords Saved</h1>}
-          {passwordarray.length > 0 && <table className="table-auto w-[70%] text-white mx-auto mt-4 rounded-t-xl overflow-hidden ">
-            <thead className='bg-green-600 text-white '>
+          {passwordarray.length > 0 && <table className="table-fixed w-[70%] text-white mx-auto mt-4 rounded-t-xl overflow-hidden border-separate border-spacing-0">
+            <thead className='bg-green-600 text-white'>
               <tr>
-                <th>Website</th>
-                <th>Username</th>
-                <th>Password</th>
+                <th className='py-2'>Website</th>
+                <th className='py-2'>Username</th>
+                <th className='py-2'>Password</th>
+                <th className='py-2'>Actions</th>
               </tr>
             </thead>
-            <tbody className='bg-green-100 text-black '>
-              {passwordarray.map((item,index)=>{
-                return <tr className='' key={index}>
-                <td className='text-center py-1 w-36 hover:bg-green-200 transition-all duration-300 cursor-pointer'>{item.site}</td>
-                <td className='text-center py-1 w-36'>{item.username}</td>
-                <td className='text-center py-1 w-36'>{item.password}</td>
-              </tr>
+            <tbody className='bg-green-100 text-black'>
+              {passwordarray.map((item, index) => {
+                return <tr className='border-b border-green-300' key={index}>
+                  <td className='text-center py-2 px-2'>
+                    <div className='flex items-center justify-center gap-4'>
+                      <span className='truncate'>{item.site}</span>
+                      <img width={22} src="./icons/copy.svg" alt="" className='cursor-pointer' onClick={() => copytext(item.site)} />
+                    </div>
+                  </td>
+                  <td className='text-center py-2 px-2'>
+                    <div className='flex items-center justify-center gap-4'>
+                      <span className='truncate'>{item.username}</span>
+                      <img width={22} src="./icons/copy.svg" alt="" className='cursor-pointer' onClick={() => copytext(item.username)} />
+                    </div>
+                  </td>
+                  <td className='text-center py-2 px-2'>
+                    <div className='flex items-center justify-center gap-4'>
+                      <span className='truncate'>{item.password}</span>
+                      <img width={22} src="./icons/copy.svg" alt="" className='cursor-pointer' onClick={() => copytext(item.password)} />
+                    </div>
+                  </td>
+                  <td className='text-center py-2 px-2 flex items-center justify-center gap-4'>
+                    <span><img width={28} src="./icons/delete.svg" alt="" className='cursor-pointer' onClick={() => deletePassword(item.id)} /></span>
+                    <span><img width={28} src="./icons/edit.svg" alt="" className='cursor-pointer' onClick={() => editPassword(item.id)} /></span>
+                  </td>
+                </tr>
               })}
             </tbody>
           </table>
-            }
+          }
         </div>
       </div>
     </>
