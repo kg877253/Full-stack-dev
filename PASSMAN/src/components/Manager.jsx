b644b9bc27d2edc682671c98b63a230e37cb1168
Kartik Gupta <kg877253@gmail.com>
2026-09-12 17:49:58 +0530

    button and eye click changes svg

--- PASSMAN/src/components/Manager.jsx
+++ PASSMAN/src/components/Manager.jsx
@@ -8,7 +8,7 @@ const Manager = () => {
   const showpassword = () => {
     if (ref.current.src.includes("eyecross")) {
       ref.current.src = "./icons/eyes.svg"
-    } 
+    }
     else {
       ref.current.src = "./icons/eyecross.svg"
     }
@@ -35,17 +35,22 @@ const Manager = () => {
         <p className='text-center mt-3'>Manage your passwords with ease</p>
         <div className="container mx-auto mt-10 flex flex-col gap-5 p-5 text-black items-center">
           <input onChange={savechange} value={form.site} type="text " placeholder="Website" className='rounded-md p-2 w-[50%] bg-green-100 border-3 border-green-600' name='site' />
+
           <div className="userpass bg-green-300/50 p-2 rounded-md flex gap-8 w-[50%] items-center justify-between">
             <input onChange={savechange} value={form.username} type="text" placeholder="Username" className='rounded-md p-2 bg-green-100 w-[70%] border-2 border-green-800' name='username' />
+
             <div className='relative'>
               <input onChange={savechange} value={form.password} type="text" placeholder="Password" className='w-full rounded-md p-2 bg-green-100 border-2 border-green-800' name='password' />
               <span className='absolute right-2 top-2 cursor-pointer'>
                 <img ref={ref} width={30} src="./icons/eyes.svg" alt="" onClick={showpassword} /></span>
             </div>
+
           </div>
+
           <button className='bg-green-500 hover:bg-green-400 cursor-pointer text-white font-bold py-2 px-2 rounded-2xl w-[13%] flex items-center justify-center gap-2' onClick={savepassword}>
             <img width={30} src="./icons/add.svg" alt="" />
-            Add Password</button>
+            Add Password
+          </button>
         </div>
       </div>
     </>
