f89d47615d2d3aa6552ecde811a32208f16a8bfc
Kartik Gupta <kg877253@gmail.com>
2026-09-13 16:52:07 +0530

    added responsiveness to the navbar and added borders to the table in the manager component

--- PASSMAN/src/components/Manager.jsx
+++ PASSMAN/src/components/Manager.jsx
@@ -137,58 +137,59 @@ const Manager = () => {
         </h1>
         <p className='text-center mt-3'>Manage your passwords with ease</p>
         <div className="container mx-auto mt-10 flex flex-col gap-5 p-5 text-black items-center">
-          <input onChange={savechange} value={form.site} type="text " placeholder="Website" className='rounded-md p-2 w-[50%] bg-green-100 border-3 border-green-600' name='site' />
+          <input onChange={savechange} value={form.site} type="text " placeholder="Website" className='rounded-md p-2 w-[90%] md:w-[50%] bg-green-100 border-3 border-green-600' name='site' />
 
-          <div className="userpass bg-green-300/50 p-2 rounded-md flex gap-8 w-[50%] items-center justify-between">
-            <input onChange={savechange} value={form.username} type="text" placeholder="Username" className='rounded-md p-2 bg-green-100 w-[70%] border-2 border-green-800' name='username' />
-
+          <div className="userpass bg-green-300/50 p-2 rounded-md flex gap-8 w-[90%] md:w-[50%] items-center justify-between">
+            <input onChange={savechange} value={form.username} type="text" placeholder="Username" className='rounded-md p-2 bg-green-100 w-[75%] md:w-[70%] border-2 border-green-800' name='username' />
             <div className='relative'>
               <input ref={passwordRef} onChange={savechange} value={form.password} type="text" placeholder="Password" className='w-full rounded-md p-2 bg-green-100 border-2 border-green-800' name='password' />
-              <span className='absolute right-2 top-2 cursor-pointer'>
-                <img ref={ref} width={30} src="./icons/eyes.svg" alt="" onClick={showpassword} /></span>
+              <span className='absolute right-1 md:right-1.5 top-2.5 md:top-1.5 cursor-pointer'>
+                <img ref={ref} className='md:w-8 w-6' src="./icons/eyes.svg" alt="" onClick={showpassword} /></span>
             </div>
           </div>
 
           <button className='bg-green-500 hover:bg-green-400 cursor-pointer text-white font-bold py-2 px-4 rounded-2xl max-w-fit min-w-fit flex items-center justify-center gap-2' onClick={savepassword}>
-            <img width={25} src="./icons/addgif.gif" alt="" />
-            Add Password
+            <img width={25} height={25} src="./icons/addgif.gif" alt="" />
+            SAVE
           </button>
 
         </div>
         <div>
-          <div className='text-center text-3xl font-extrabold mt-1 underline decoration-dashed decoration-green-200'>Your Passwords</div>
+          <div className='text-center text-3xl font-extrabold mt-1 md:underline decoration-dashed decoration-green-200'>Your Passwords</div>
           {passwordarray.length === 0 && <h1 className='text-center text-2xl mt-10'>No Passwords Saved</h1>}
-          {passwordarray.length > 0 && <table className="table-fixed w-[70%] text-white mx-auto mt-4 rounded-t-xl rounded-b-xl overflow-hidden border-separate border-spacing-0">
+          {passwordarray.length > 0 && <table className="table-fixed w-[90%] md:w-[70%] text-white mx-auto mt-4 rounded-t-xl rounded-b-xl overflow-hidden border-separate border-spacing-0">
             <thead className='bg-green-600 text-white'>
+
               <tr>
-                <th className='py-2'>Website</th>
-                <th className='py-2'>Username</th>
-                <th className='py-2'>Password</th>
-                <th className='py-2'>Actions</th>
+                <th className='py-2 border-r border-black'>Website</th>
+                <th className='py-2 border-r border-black'>Username</th>
+                <th className='py-2 border-r border-black'>Password</th>
+                <th className='py-2 border-r border-black'>Actions</th>
               </tr>
+
             </thead>
             <tbody className='bg-green-100 text-black'>
               {passwordarray.map((item, index) => {
-                return <tr className='border-b border-green-300' key={index}>
-                  <td className='text-center py-2 px-2'>
+                return <tr className='' key={index}>
+                  <td className='text-center py-2 px-2 border-r border-black'>
                     <div className='flex items-center justify-center gap-4'>
                       <span className='truncate'>{item.site}</span>
-                      <img width={22} src="./icons/copy.svg" alt="" className='cursor-pointer' onClick={() => copytext(item.site)} />
+                      <img className='cursor-pointer w-4 md:w-6' src="./icons/copy.svg" alt=""  onClick={() => copytext(item.site)} />
                     </div>
                   </td>
-                  <td className='text-center py-2 px-2'>
+                  <td className='text-center py-2 px-2 border-r border-black'>
                     <div className='flex items-center justify-center gap-4'>
                       <span className='truncate'>{item.username}</span>
-                      <img width={22} src="./icons/copy.svg" alt="" className='cursor-pointer' onClick={() => copytext(item.username)} />
+                      <img className='cursor-pointer w-4 md:w-6' src="./icons/copy.svg" alt=""  onClick={() => copytext(item.username)} />
                     </div>
                   </td>
-                  <td className='text-center py-2 px-2'>
+                  <td className='text-center py-2 px-2 border-r border-black'>
                     <div className='flex items-center justify-center gap-4'>
                       <span className='truncate'>{item.password}</span>
-                      <img width={22} src="./icons/copy.svg" alt="" className='cursor-pointer' onClick={() => copytext(item.password)} />
+                      <img className='cursor-pointer w-4 md:w-6' src="./icons/copy.svg" alt="" onClick={() => copytext(item.password)} />
                     </div>
                   </td>
-                  <td className='text-center py-2 px-2 flex items-center justify-center gap-4'>
+                  <td className='text-center py-2 px-2 flex items-center justify-center gap-4 border-r border-black'>
                     <span><img width={28} src="./icons/delete.svg" alt="" className='cursor-pointer' onClick={() => deletePassword(item.id)} /></span>
                     <span><img width={28} src="./icons/edit.svg" alt="" className='cursor-pointer' onClick={() => editPassword(item.id)} /></span>
                   </td>
--- PASSMAN/src/components/Navbar.jsx
+++ PASSMAN/src/components/Navbar.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='bg-gray-800 flex flex-row gap-0 items-center justify-between px-10 text-white h-16 sticky top-0 z-50'>
+    <nav className='bg-gray-800 hover:bg-gray-800/40 flex flex-row items-center justify-between md:px-10 px-1.5 text-white h-16 sticky top-0 z-50'>
         <div className='text-3xl font-medium'>
             <span className='text-green-500'> &lt;</span>
             <span className=''>Pass</span>
             <span className='text-green-500'>MAN/&gt;</span>
         </div>
 
-        <div className='flex flex-row gap-5 text-lg items-center'>
+        <div className='flex flex-row md:gap-4 text-lg items-center'>
           <a href="https://github.com/kg877253/Full-stack-dev/tree/main/PASSMAN" target="_blank" rel="noopener noreferrer"
-                    className='flex items-center gap-2 bg-green-600 hover:bg-green-500 px-3 py-2 rounded-md transition-colors'
+                    className='flex items-center overflow-auto sm:p-1.5 md:gap-2 bg-green-600 hover:bg-green-500 md:px-3 md:py-2 rounded-md transition-colors'
                 >
                     <img src="./icons/github.svg" alt="GitHub" />
                     <span className='text-base'>GitHub</span>
