3b24a78791fcfcd6eb0926b7a7ae83fc161eda44
Kartik Gupta <kg877253@gmail.com>
2026-09-18 16:12:20 +0530

    making payment buttons# Please enter the commit message for your changes. Lines starting

--- GETMECHAI/get-me-chai/app/layout.js
+++ GETMECHAI/get-me-chai/app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
       <body className="min-h-screen flex flex-col">
         <SessionWrapper>
           <Navbar />
-          <div className="min-h-[85vh] text-white">
+          <div className=" text-white">
             <div className=" fixed top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
             {children}
           </div>
