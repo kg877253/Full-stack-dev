7cf33785df25f7581b3cdb0047c073c28bdca54e
Kartik Gupta <kg877253@gmail.com>
2026-09-18 19:14:53 +0530

    added user in database

--- GETMECHAI/get-me-chai/app/api/auth/[...nextauth]/route.js
+++ GETMECHAI/get-me-chai/app/api/auth/[...nextauth]/route.js
@@ -1,50 +1,56 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-// import GoogleProvider from "next-auth/providers/google"
-import User from "@/models/user";
-import Payment from "@/models/payment";
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import mongoose from "mongoose";
 
-const handler = NextAuth(
-    {
-        // Configure one or more authentication providers
-        providers: [
-            GithubProvider({
-                clientId: process.env.GITHUB_ID,
-                clientSecret: process.env.GITHUB_SECRET,
-            }),
-            // GoogleProvider({
-            //     clientId: process.env.GOOGLE_ID,
-            //     clientSecret: process.env.GOOGLE_SECRET,
-            // }),
-            // ...add more providers here
-        ],
-        callbacks: {
-            async signIn({ user,account, profile,email, credentials }) {
-               if (account.providers === "github") {
-                const client = await mongoose.connect("mongodb://localhost:27017/getchai");
-                const currentuser= await User.findOne({ email: user.email });
-                if(!currentuser){
+import User from "@/models/user";
+
+const handler = NextAuth({
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+    ],
+
+    callbacks: {
+        async signIn({ user, account }) {
+
+            if (account.provider === "github") {
+
+                await mongoose.connect(
+                    "mongodb://localhost:27017/getchai"
+                );
+
+                const currentuser = await User.findOne({
+                    email: user.email
+                });
+
+                if (!currentuser) {
                     const newuser = await User.create({
-                        email:email,
-                        username:user.split("@")[0]
+                        email: user.email,
+                        username: user.email.split("@")[0]
                     });
-                    await newuser.save();
                     console.log("New user created:", newuser);
+
                     user.name = newuser.username;
-                }
-                else{
-                    user.name = currentuser.username;
+                    console.log("User name set to:", user.name);
                 }
                 return true;
-               }
-            },
-            async session({ session, token, user }) {
-                const dbuser= await User.findOne({ email: session.user.email });
-                session.user.username= dbuser.username;
-                return session;
             }
+            return false;
+        },
+
+        async session({ session }) {
+
+            const dbuser = await User.findOne({
+                email: session.user.email
+            });
+            if (dbuser) {
+                session.user.username = dbuser.username;
+            }
+            return session;
         }
     }
-)
+});
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
