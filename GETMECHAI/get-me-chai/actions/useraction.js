eb4f1985dbdc719c9d81314a59d0ba3bd19af346
Kartik Gupta <kg877253@gmail.com>
2026-09-19 14:51:36 +0530

    doing razorpay integration and adding mongodb connection string in .env.local file

--- GETMECHAI/get-me-chai/actions/useraction.js
+++ GETMECHAI/get-me-chai/actions/useraction.js
@@ -1,1 +1,18 @@
 "use server"
+
+import Razorpay from "razorpay"
+import dbConnect from "@/db/connect"
+import payment from "@/models/payment"
+import user from "@/models/user"
+
+export const initiatePayment = async (amount, to_username, paymentform) => {
+    await dbConnect()
+
+    var instance = new Razorpay({ key_id: 'YOUR_KEY_ID', key_secret: 'YOUR_SECRET' })
+
+    let options={
+        amount: amount,
+        currency: "INR",
+    }
+    
+}
