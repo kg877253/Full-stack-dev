6d3076cf63c9686030b665a7204de3bbc5dac3d9
Kartik Gupta <kg877253@gmail.com>
2026-09-23 17:44:58 +0530

    Seperating the project GETMECHAI from this repo

--- GETMECHAI/get-me-chai/app/login/layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: 'Login - GetMeAChai',
-  description: 'Log in to support creators or manage your GetMeAChai page.',
-}
-
-export default function LoginLayout({ children }) {
-  return children
-}
